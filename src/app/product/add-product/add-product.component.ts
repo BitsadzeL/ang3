01c39958af9917cc6ab0afaa28be12e3136fc660
Luka Bitsadze <bitsadzeluka@gmail.com>
2023-12-28 21:15:13 +0400

    added-guard

--- src/app/product/add-product/add-product.component.ts
+++ src/app/product/add-product/add-product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import {  Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { ProductService } from 'src/app/services/product.service';
 
 
@@ -9,38 +9,47 @@ import { ProductService } from 'src/app/services/product.service';
   templateUrl: './add-product.component.html',
   styleUrls: ['./add-product.component.css']
 })
-export class AddProductComponent  implements OnInit{
-  constructor(private router:Router, private fb:FormBuilder, private prodService:ProductService ){}
-  productForm!:FormGroup;
-  newIsSaved!:boolean;
+export class AddProductComponent implements OnInit {
+  constructor(private router: Router, private fb: FormBuilder, private prodService: ProductService) { }
+  productForm!: FormGroup;
+  newIsSaved!: boolean;
 
-  ngOnInit():void{
-    this.newIsSaved=false;
+  ngOnInit(): void {
+    this.newIsSaved = false;
     this.createForm();
   }
 
-  createForm(){
-    this.productForm=this.fb.group({
-      name:[''],
-      price:['']
+  createForm() {
+    this.productForm = this.fb.group({
+      name: [''],
+      price: ['']
     })
   }
-  
 
 
-  goToMain(){
+
+  goToMain() {
     this.router.navigate(['home']);
   }
 
-  goToDashboard(){
+  goToDashboard() {
     this.router.navigate(['product/productdashboard']);
   }
- 
+
 
   onSubmit() {
     const productData = this.productForm.value;
     this.prodService.addProduct(productData);
-    this.newIsSaved=true;
+    this.newIsSaved = true;
+  }
+
+  canExit() {
+    if (!this.newIsSaved) {
+      return window.confirm('You have unsaved changes. Do you really want to discard this change?');
+
+    } else {
+      return true;
+    }
   }
 
 
--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProductComponent } from './add-product/add-product.component';
 import { ProductDashboardComponent } from './product-dashboard/product-dashboard.component';
-import { HomeComponent } from '../home/home.component';
+import { ProductDeGuard } from '../guards/product-de.guard';
 
 
 const routes: Routes = [
-  { path: 'addproduct', component: AddProductComponent },
+  { path: 'addproduct', component: AddProductComponent,canDeactivate:[ProductDeGuard] },
   { path: 'productdashboard', component: ProductDashboardComponent },
   { path: '',  redirectTo:"/productdashboard", pathMatch:"full"},
 ];
